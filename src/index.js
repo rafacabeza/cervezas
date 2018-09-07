da0916869824803d90f197d8f2dd94a805448030
Rafa Cabeza <rafacabeza@gmail.com>
2018-09-07 13:27:20 +0200

    ordenar cervezas en todas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const cervezas = require('./cervezas.json')
 const uniqueRandomArray = require('unique-random-array')
+const _ = require('lodash')
 
 module.exports = {
-  todas: cervezas,
+  todas: _.sortBy(cervezas, ['nombre']),
   preferida: 'ambar que es de casa',
   alazar: uniqueRandomArray(cervezas)
 }
